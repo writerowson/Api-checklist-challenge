96ed839a38c0f7e1dc08700671009de11389e533
coderowson <runurowson@gmail.com>
2022-03-14 13:23:49 +0600

    problem05

--- js/problem01.js
+++ js/problem01.js
@@ -1,32 +1,11 @@
 // ১. একদম প্রাথমিক স্টেপ হিসেবে jsonplaceholder এর ওয়েবসাইট থেকে ডাটা fetch করে সেটাকে কনসোল এ দেখাতে হবে। ধরো তুমি তাদের ওয়েবসাইট এ comments এর API এর লিংক কোনটা সেটা জাভাস্ক্রিপ্ট দিয়ে কোড করে সেই ডাটা কনসোল এ দেখতে পারতেছো কিনা। 
 
-const output = () => {
-    fetch('https://jsonplaceholder.typicode.com/comments')
-        .then(res => res.json())
-        .then(data => commentBtn(data))
-}
-output();
+fetch('https://jsonplaceholder.typicode.com/comments')
+    .then(res => res.json())
+    .then(data => console.log(data))
 
-// ২. যে কমেন্ট এর ডাটাগুলো কনসোল এ দেখাতে পারছো। সেগুলা আবার তুমি html এ দেখাতে পারতেছো কিনা। একটা সিস্টেম হবে তোমার একটা বাটন। থাকবে সেটাতে ক্লিক করলে ডাটা লোড হবে। তারপর সেই ডাটা তুমি ওয়েবসাইট এ দেখাবে।
-
-const commentBtn = (show) => {
-    console.log(show)
-    const mainResult = document.getElementById('main');
-    const frist10dData = show.slice(0, 10)
-
-    for (const output of frist10dData) {
-        const div = document.createElement('div')
-        div.innerHTML = `
-        <h2>${output.name}</h2>
-        `;
-        mainResult.appendChild(div)
-    }
-}
 
 // conceptional session 1.03.03
-
-// ৩. আরেকটা সিস্টেম হবে। তুমি যে ডাটা লোড করেছো। সেটা কোন বাটনে ক্লিক করা লাগবে না। ওয়েবসাইট লোড হলেই সরাসরি ডাটা লোড হয়ে তারপর সেই ডাটা ওয়েবসাইট এ দেখাবে। চেষ্টা করবে লোড করা ডাটা এর দুইটা প্রপার্টি অবশ্যই দেখাবে। 
-
 
 
 
---
+++ js/problem02.js
@@ -0,0 +1,41 @@
+
+// ২. যে কমেন্ট এর ডাটাগুলো কনসোল এ দেখাতে পারছো। সেগুলা আবার তুমি html এ দেখাতে পারতেছো কিনা। একটা সিস্টেম হবে তোমার একটা বাটন। থাকবে সেটাতে ক্লিক করলে ডাটা লোড হবে। তারপর সেই ডাটা তুমি ওয়েবসাইট এ দেখাবে।
+const output = () => {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+        .then(res => res.json())
+        .then(data => commentBtn(data))
+}
+// function commentBtn(show) {
+//     const mainResult = document.getElementById('main');
+//     const frist10dData = show.slice(0, 10)
+//     for (const output of frist10dData) {
+//         const div = document.createElement('div')
+//         div.innerHTML = `
+//         <h2>${output.name}</h2>
+//         <h2>${output.body}</h2>
+//         `;
+//         mainResult.appendChild(div)
+//     }
+// }
+// ৩. আরেকটা সিস্টেম হবে। তুমি যে ডাটা লোড করেছো। সেটা কোন বাটনে ক্লিক করা লাগবে না। ওয়েবসাইট লোড হলেই সরাসরি ডাটা লোড হয়ে তারপর সেই ডাটা ওয়েবসাইট এ দেখাবে। চেষ্টা করবে লোড করা ডাটা এর দুইটা প্রপার্টি অবশ্যই দেখাবে।
+
+output();
+
+// ৪. তুমি যে ডাটা লোড করেছো। বা ডাটা ওয়েবসাইট এ দেখাচ্ছ। সেই কোড এর মধ্যে arrow ফাংশন ইউজ করতে পারতেছো কিনা। যখন লুপ চালাচ্ছ সেখানে forEach ইউজ করতে পারতেছো কিনা।
+
+const commentBtn = (showUI) => {
+    const mainResult = document.getElementById('main');
+    const first10Data = showUI.slice(0, 10)
+    console.log(first10Data)
+    first10Data.forEach(showUI => {
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <h2>${showUI.name}</h2>
+        `;
+        mainResult.appendChild(div)
+    });
+}
+
+// ৫. ডাইনামিক ডাটা লোড। কোন একটা কমেন্ট এ ক্লিক করলে (কমেন্ট এর div এ বা কোন একটা বাটন এ )সেই কমেন্ট এর আইডি নিয়ে সেটা api এর url এ বসিয়ে (ডাইনামিকভাবে টেমপ্লেট স্ট্রিং দিয়ে) সেই ডাটা লোড করে। সেই ডাটা ওয়েবসাইট এ দেখাতে পারতেছো কিনা।
+
+// ৬. randomuser এর ওয়েবসাইট এ গিয়ে (randomuser.me) এ গিয়ে সেখান থেকে ডাটা লোড করবে। তারপর ইউজারের ছবি দেখাবে। শুধু সেটাও না। ইউজারের location এর মধ্যে যত কিছু আছে। সব দেখাবে ওয়েবসাইট এ। অর্থাৎ street, city, coordinates, timezone যেকোন একভাবে দেখলেই হবে। তবে দেখাতে হবে। 
